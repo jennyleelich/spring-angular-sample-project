0deab004ba06383a3ef86fb62a80167a40f01733
chunhua li <chunhuali@chunhuas-iMac.hsd1.nj.comcast.net>
2023-05-05 19:03:30 -0400

    modify pom.xml dependency sequence to solve maven build issue and adding access token to auth request header in  http intercepter to rest api

--- release 1.0/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ release 1.0/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ProductService } from './service/product.service';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
@@ -28,6 +28,7 @@ import { OktaAuth } from '@okta/okta-auth-js';
 import myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { AuthInterceptorService } from './service/auth-interceptor.service';
 const oktaConfig = myAppConfig.oidc;
 const oktaAuth = new OktaAuth(oktaConfig);
 function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector) {
@@ -77,7 +78,8 @@ const routes: Routes = [
         ReactiveFormsModule,
         OktaAuthModule
     ],
-    providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth } }],
+    providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth } },
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
