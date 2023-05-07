25c84f212d3349d80de03a9c19fcb2ca37c29e12
chunhua li <chunhuali@chunhuas-iMac.hsd1.nj.comcast.net>
2023-05-07 14:05:49 -0400

    config swagger property title version desc etc

--- release 1.0/02-backend/spring-boot-ecommerce/src/main/java/com/example/springbootecommerce/SpringBootEcommerceApplication.java
+++ release 1.0/02-backend/spring-boot-ecommerce/src/main/java/com/example/springbootecommerce/SpringBootEcommerceApplication.java
@@ -1,9 +1,30 @@
 package com.example.springbootecommerce;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Jenny's Shop App",
+				version = "1.0.0",
+				description = "Spring Boot 3.0.2 project",
+				termsOfService = "happy coding",
+				contact = @Contact(
+						name = "Jenny Li",
+						email = "chunhua.li.chen@gmail.com"),
+				license = @License(
+					name = "license",
+					url = "happyCoding")
+				
+				) )
+
 public class SpringBootEcommerceApplication {
 
 	public static void main(String[] args) {
